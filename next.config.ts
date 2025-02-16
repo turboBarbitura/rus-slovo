b8c0dcebb808ad7eb2cf6564817e79c0e4642db8
Mikhail Shchukin <turbobarbitura@mail.ru>
2025-02-16 21:03:03 +0700

    feat(deploy): actions config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    trailingSlash: true, // обязательно для корректного маршрутизатора
 };
 
 export default nextConfig;
